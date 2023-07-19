b9405a12f665534fea13ea32141e2714cfda9295
lastonton <138580872+lastonton@users.noreply.github.com>
2023-07-19 20:20:15 +0700

    new code

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.i3pkmcl.mongodb.net',
+    DB_USER: 'nhannguyen2703',
+    DB_PASS: '1234567nhan',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'nhannguyen27032002@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'SIUNHANGAO12@n123',
+    JWT_SECRET: 'cothieuhoang',
+    JWT_EXPIRES: '600000', // in milliseconds
+};
+module.exports = MyConstants;
